73aa32f9be9ad871779878e4343c3ed818f67a06
Jon Hodgins <19355502+JonHodgins@users.noreply.github.com>
2022-02-28 15:41:53 -0700

    Add data project and species entities

---
+++ src/WildlifeMortalities.Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WildlifeMortalities.Data.Entities;
+
+namespace WildlifeMortalities.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Bison> Bisons => Set<Bison>();
+        public DbSet<BlackBear> BlackBears => Set<BlackBear>();
+        public DbSet<Caribou> Caribous => Set<Caribou>();
+        public DbSet<Coyote> Coyotes => Set<Coyote>();
+        public DbSet<Deer> Deers => Set<Deer>();
+        public DbSet<Elk> Elks => Set<Elk>();
+        public DbSet<Goat> Goats => Set<Goat>();
+        public DbSet<GrizzlyBear> GrizzlyBears => Set<GrizzlyBear>();
+        public DbSet<Moose> Moose => Set<Moose>();
+        public DbSet<Sheep> Sheep => Set<Sheep>();
+        public DbSet<Wolf> Wolves => Set<Wolf>();
+        public DbSet<Wolverine> Wolverines => Set<Wolverine>();
+    }
+}
