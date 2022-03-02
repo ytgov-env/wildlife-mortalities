c4fa7b3b39a800cb77b4803bd25d289156ed9f12
Jon Hodgins <19355502+JonHodgins@users.noreply.github.com>
2022-03-01 17:32:17 -0700

    Add legacy dbcontext and entities

---
+++ src/WildlifeMortalities.Data/Legacy/LegacyDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WildlifeMortalities.Data.Legacy
+{
+    public class LegacyDbContext
+    {
+        private static string connString = "Data Source=sql-apps2-prd.ynet.gov.yk.ca;Initial Catalog=HarvestDB;Integrated Security=True;";
+
+        public static IDbConnection GetConnection()
+        {
+            var conn = new SqlConnection(connString);
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            return conn;
+        }
+
+        public static void CloseConnection(IDbConnection conn)
+        {
+            if (conn.State == ConnectionState.Open || conn.State == ConnectionState.Broken)
+            {
+                conn.Close();
+            }
+        }
+    }
+}
