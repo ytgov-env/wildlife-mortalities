46129dac3f116a021a55b1951330f581b09d4536
JonHodgins <19355502+JonHodgins@users.noreply.github.com>
2022-04-13 15:55:08 -0700

    Add DateStarted and DateCompleted properties to harvest reports

--- src/WildlifeMortalities.Data/Entities/HarvestReports/HarvestReportBase.cs
+++ src/WildlifeMortalities.Data/Entities/HarvestReports/HarvestReportBase.cs
@@ -3,6 +3,7 @@
 public class HarvestReportBase
 {
     public int Id { get; set; }
-    public DateTime DateReported { get; set; }
+    public DateTimeOffset DateStarted { get; set; }
+    public DateTimeOffset DateCompleted { get; set; }
     public bool IsComplete { get; set; }
 }
