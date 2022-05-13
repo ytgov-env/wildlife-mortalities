13987343adc30716308527c0a24c0aa8ed6bda9a
JonHodgins <19355502+JonHodgins@users.noreply.github.com>
2022-05-13 16:07:04 -0700

    Reorganize properties in GameManagementAreaSpecies

--- src/WildlifeMortalities.Data/Entities/GameManagementAreaSpecies.cs
+++ src/WildlifeMortalities.Data/Entities/GameManagementAreaSpecies.cs
@@ -5,8 +5,8 @@ namespace WildlifeMortalities.Data.Entities;
 public class GameManagementAreaSpecies
 {
     public int Id { get; set; }
-    public int GameManagementAreaId { get; set; }
     public GmaSpecies Species { get; set; }
+    public int GameManagementAreaId { get; set; }
     public GameManagementArea GameManagementArea { get; set; } = null!;
     public List<GameManagementAreaSchedule> Schedules { get; set; } = new();
     public List<GameManagementUnit?> GameManagementUnits { get; set; } = new();
