0477c2bf9375dd3e973364b5b673fefe3fc81750
JonHodgins <19355502+JonHodgins@users.noreply.github.com>
2022-05-30 10:48:21 -0700

    Add season to licence entity

--- src/WildlifeMortalities.Data/Entities/Licences/Licence.cs
+++ src/WildlifeMortalities.Data/Entities/Licences/Licence.cs
@@ -7,6 +7,9 @@ public class Licence
 {
     public int Id { get; set; }
     public string Number { get; set; } = string.Empty;
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate{ get; set; }
+    public string Season => $"{StartDate.Year}-{EndDate.Year}";
     public int ClientId { get; set; }
     public Client Client { get; set; } = null!;
 }
