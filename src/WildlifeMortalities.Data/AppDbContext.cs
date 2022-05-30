80fc7669824f6e96a18434837e2f3489e0594e5f
JonHodgins <19355502+JonHodgins@users.noreply.github.com>
2022-05-30 10:35:38 -0700

    Remove onconfiguring override, and derived type dbsets from appdbcontext

--- src/WildlifeMortalities.Data/AppDbContext.cs
+++ src/WildlifeMortalities.Data/AppDbContext.cs
@@ -9,22 +9,17 @@ namespace WildlifeMortalities.Data;
 
 public class AppDbContext : DbContext
 {
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
     public DbSet<Reporter> Reporters => Set<Reporter>();
-    public DbSet<Client> Clients => Set<Client>();
-    public DbSet<ConservationOfficer> ConservationOfficers => Set<ConservationOfficer>();
 
     public DbSet<Licence> Licences => Set<Licence>();
     public DbSet<Seal> Seals => Set<Seal>();
 
     public DbSet<Mortality> Mortalities => Set<Mortality>();
-    public DbSet<BisonMortality> BisonMortalities => Set<BisonMortality>();
-    public DbSet<BlackBearMortality> BlackBearMortalities => Set<BlackBearMortality>();
-
-
-
     public DbSet<HarvestReport> HarvestReports => Set<HarvestReport>();
-    public DbSet<TrappedHarvestReport> TrappedHarvestReports => Set<TrappedHarvestReport>();
-    public DbSet<HuntedHarvestReport> HuntedHarvestReports => Set<HuntedHarvestReport>();
 
     public DbSet<BioSubmission> BioSubmissions => Set<BioSubmission>();
 
@@ -32,14 +27,6 @@ public class AppDbContext : DbContext
     public DbSet<GameManagementAreaSpecies> GameManagementAreaSpecies => Set<GameManagementAreaSpecies>();
     public DbSet<GameManagementAreaSchedule> GameManagementAreaSchedules => Set<GameManagementAreaSchedule>();
     public DbSet<GameManagementUnit> GameManagementUnits => Set<GameManagementUnit>();
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=EnvWildlifeMortalities;Integrated Security=True;",
-            x => x.UseNetTopologySuite())
-            .UseEnumCheckConstraints();
-        base.OnConfiguring(optionsBuilder);
-    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -54,15 +41,30 @@ public class AppDbContext : DbContext
         modelBuilder.Entity<Mortality>().HasOne(m => m.Reporter)
             .WithMany(r => r.Mortalities).OnDelete(DeleteBehavior.NoAction);
 
-        modelBuilder.Entity<GameManagementAreaSpecies>().Property(s => s.Species).HasConversion<string>().HasMaxLength(25);
+        modelBuilder.Entity<WoodBisonMortality>(b =>
+        {
+            b.Property(b => b.TemporarySealNumber).HasColumnName(nameof(WoodBisonMortality.TemporarySealNumber));
+            b.Property(b => b.Sex).HasConversion<string>().HasColumnName(nameof(WoodBisonMortality.Sex));
+            b.Property(b => b.GameManagementAreaId).HasColumnName(nameof(WoodBisonMortality.GameManagementAreaId));
+            b.Property(b => b.Landmark).HasColumnName(nameof(WoodBisonMortality.Landmark));
+            b.Property(b => b.Coordinates).HasColumnName(nameof(WoodBisonMortality.Coordinates));
+        });
+
+        modelBuilder.Entity<AmericanBlackBearMortality>(b =>
+        {
+            b.Property(b => b.TemporarySealNumber).HasColumnName(nameof(AmericanBlackBearMortality.TemporarySealNumber));
+            b.Property(b => b.Sex).HasConversion<string>().HasColumnName(nameof(AmericanBlackBearMortality.Sex));
+            b.Property(b => b.GameManagementAreaId).HasColumnName(nameof(AmericanBlackBearMortality.GameManagementAreaId));
+            b.Property(b => b.Landmark).HasColumnName(nameof(AmericanBlackBearMortality.Landmark));
+            b.Property(b => b.Coordinates).HasColumnName(nameof(AmericanBlackBearMortality.Coordinates));
+        });
 
         modelBuilder.Entity<GameManagementArea>().Property(a => a.Area).HasComputedColumnSql("[Zone] + [Subzone]", true);
-
+        modelBuilder.Entity<GameManagementAreaSpecies>().Property(s => s.Species).HasConversion<string>().HasMaxLength(25);
         modelBuilder.Entity<GameManagementAreaSchedule>().Property(s => s.PeriodStart).HasColumnType("date");
         modelBuilder.Entity<GameManagementAreaSchedule>().Property(s => s.PeriodEnd).HasColumnType("date");
         modelBuilder.Entity<GameManagementUnit>().Property(u => u.ActiveFrom).HasColumnType("date");
         modelBuilder.Entity<GameManagementUnit>().Property(u => u.ActiveTo).HasColumnType("date");
-
         modelBuilder.Entity<GameManagementUnit>().Property(u => u.Name).HasMaxLength(50);
 
         // These shadow properties are referenced during ETL to sync licence and seal data from their source (POSSE)
