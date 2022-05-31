df02faf79c63609e9de7e5a0778a39265c931cf8
JonHodgins <19355502+JonHodgins@users.noreply.github.com>
2022-05-31 16:54:16 -0700

    Add validators for mortality and huntedharvestreport entities

---
+++ src/WildlifeMortalities.Shared/Validators/HuntedHarvestReportValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using WildlifeMortalities.Data.Entities;
+
+namespace WildlifeMortalities.Shared.Validators;
+
+public class HuntedHarvestReportValidator<T> : AbstractValidator<HuntedHarvestReport>
+{
+    public HuntedHarvestReportValidator()
+    {
+        RuleFor(h => h.Mortality).NotNull().SetValidator(new MortalityValidator<T>());
+        RuleFor(h => h.TemporarySealNumber)
+            .Must(
+                (harvestReport, temporarySealNumber) =>
+                    harvestReport.Seal == null || temporarySealNumber.Length >= 5
+            )
+            .WithMessage(
+                "The harvest report must be associated with a valid Seal, or have a temporary seal number"
+            );
+    }
+}
---
+++ src/WildlifeMortalities.Shared/Validators/MortalityValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using WildlifeMortalities.Data.Entities.Mortalities;
+
+namespace WildlifeMortalities.Shared.Validators;
+
+public class MortalityValidator<T> : AbstractValidator<Mortality>
+{
+    public MortalityValidator()
+    {
+#pragma warning disable RCS1146 // Use conditional access.
+        RuleFor(m => m.Coordinates)
+            .Must(c => c == null || c.IsValid)
+            .WithMessage("The coordinates are not topologically valid");
+#pragma warning restore RCS1146 // Use conditional access.
+        RuleFor(m => m.ReporterId).NotNull();
+    }
+}
