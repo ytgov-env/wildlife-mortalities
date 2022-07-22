9691db7ff6c95827eac3dc1c24f28dd9c934f3a9
JonHodgins <19355502+JonHodgins@users.noreply.github.com>
2022-07-22 15:42:48 -0700

    Delete bio submission classes for caribou, as caribou do not require a bio submission

--- src/WildlifeMortalities.Data/Entities/BiologicalSubmissions/BarrenGroundCaribouBioSubmission.cs
+++
@@ -1,9 +0,0 @@
-﻿using WildlifeMortalities.Data.Entities.Mortalities;
-
-namespace WildlifeMortalities.Data.Entities.BiologicalSubmissions;
-
-public class BarrenGroundCaribouBioSubmission : BioSubmission
-{
-    public int MortalityId { get; set; }
-    public BarrenGroundCaribouMortality Mortality { get; set; }
-}
--- src/WildlifeMortalities.Data/Entities/BiologicalSubmissions/WoodlandCaribouBioSubmission.cs
+++
@@ -1,9 +0,0 @@
-﻿using WildlifeMortalities.Data.Entities.Mortalities;
-
-namespace WildlifeMortalities.Data.Entities.BiologicalSubmissions;
-
-public class WoodlandCaribouBioSubmission : BioSubmission
-{
-    public int MortalityId { get; set; }
-    public WoodlandCaribouMortality Mortality { get; set; }
-}
