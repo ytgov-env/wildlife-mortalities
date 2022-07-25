97b59247980496f321cf9af1cc23c86a187920be
JonHodgins <19355502+JonHodgins@users.noreply.github.com>
2022-07-25 10:07:53 -0700

    Add properties to harvest report entities

--- src/WildlifeMortalities.Data/Entities/HarvestReports/HuntedHarvestReport.cs
+++ src/WildlifeMortalities.Data/Entities/HarvestReports/HuntedHarvestReport.cs
@@ -7,6 +7,8 @@ public class HuntedHarvestReport : HarvestReport
     public int MortalityId { get; set; }
     public Mortality Mortality { get; set; }
     public string? TemporarySealNumber { get; set; }
+    public int GmaSpeciesId { get; set; }
+    public GameManagementAreaSpecies GmaSpecies { get; set; }
     public int? SealId { get; set; }
     public Seal? Seal { get; set; }
 }
--- src/WildlifeMortalities.Data/Entities/HarvestReports/TrappedHarvestReport.cs
+++ src/WildlifeMortalities.Data/Entities/HarvestReports/TrappedHarvestReport.cs
@@ -1,8 +1,11 @@
-﻿using WildlifeMortalities.Data.Entities.Mortalities;
+﻿using WildlifeMortalities.Data.Entities.Licences;
+using WildlifeMortalities.Data.Entities.Mortalities;
 
 namespace WildlifeMortalities.Data.Entities;
 
 public class TrappedHarvestReport : HarvestReport
 {
     public List<Mortality> Mortalities { get; set; } = new();
+    public int TrappingLicenceId { get; set; }
+    public TrappingLicence TrappingLicence { get; set; }
 }
