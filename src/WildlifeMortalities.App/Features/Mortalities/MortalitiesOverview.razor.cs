dcb5cbac8163ad80b2ff90b7ad78d932521fcd90
JonHodgins <19355502+JonHodgins@users.noreply.github.com>
2022-07-25 10:07:17 -0700

    Add mortalities overview page

---
+++ src/WildlifeMortalities.App/Features/Mortalities/MortalitiesOverview.razor.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Components;
+using WildlifeMortalities.Data.Entities.Mortalities;
+using WildlifeMortalities.Shared.Services;
+
+namespace WildlifeMortalities.App.Features.Mortalities;
+
+public partial class MortalitiesOverview
+{
+    [Inject]
+    private MortalityService MortalityService { get; set; }
+
+    [Inject]
+    private NavigationManager NavigationManager { get; set; }
+
+    List<Mortality> _mortalities = new();
+
+    protected override async Task OnInitializedAsync()
+    {
+        _mortalities = await MortalityService.GetAllMortalities();
+    }
+
+    private void ReportAHarvest()
+    {
+        NavigationManager.NavigateTo("harvest-reports/create");
+    }
+}
