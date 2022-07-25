b76257ea0c267f731d8c7a1264206fd9fb8cda62
JonHodgins <19355502+JonHodgins@users.noreply.github.com>
2022-07-25 15:54:15 -0700

    Fixed namespace

--- src/WildlifeMortalities.App/Pages/Authentication/Login.cshtml.cs
+++ src/WildlifeMortalities.App/Pages/Authentication/Login.cshtml.cs
@@ -2,24 +2,23 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace WildlifeMortalities.App.Pages.Authentication
+namespace WildlifeMortalities.App.Pages.Authentication;
+
+[AllowAnonymous]
+public class LoginModel : PageModel
 {
-    [AllowAnonymous]
-    public class LoginModel : PageModel
+    private IConfiguration Configuration { get; set; }
+
+    public LoginModel(IConfiguration configuration)
     {
-        private IConfiguration Configuration { get; set; }
+        Configuration = configuration;
+    }
 
-        public LoginModel(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public async Task OnGet(string redirectUri)
-        {
-            await HttpContext.ChallengeAsync(
-                Configuration["AuthNProvider:Name"],
-                new AuthenticationProperties { RedirectUri = redirectUri }
-            );
-        }
+    public async Task OnGet(string redirectUri)
+    {
+        await HttpContext.ChallengeAsync(
+            Configuration["AuthNProvider:Name"],
+            new AuthenticationProperties { RedirectUri = redirectUri }
+        );
     }
 }
--- src/WildlifeMortalities.App/Pages/Authentication/Logout.cshtml.cs
+++ src/WildlifeMortalities.App/Pages/Authentication/Logout.cshtml.cs
@@ -2,27 +2,26 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace WildlifeMortalities.App.Pages.Authentication
+namespace WildlifeMortalities.App.Pages.Authentication;
+
+public class LogoutModel : PageModel
 {
-    public class LogoutModel : PageModel
+    private IConfiguration Configuration { get; }
+
+    public LogoutModel(IConfiguration configuration)
     {
-        private IConfiguration Configuration { get; set; }
+        Configuration = configuration;
+    }
 
-        public LogoutModel(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public async Task OnGet()
-        {
-            await HttpContext.SignOutAsync(
-                Configuration["AuthNProvider:Name"],
-                new AuthenticationProperties
-                {
-                    RedirectUri = Configuration["AuthNProvider:SignedOutCallbackPath"]
-                }
-            );
-            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-        }
+    public async Task OnGet()
+    {
+        await HttpContext.SignOutAsync(
+            Configuration["AuthNProvider:Name"],
+            new AuthenticationProperties
+            {
+                RedirectUri = Configuration["AuthNProvider:SignedOutCallbackPath"]
+            }
+        );
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
     }
 }
