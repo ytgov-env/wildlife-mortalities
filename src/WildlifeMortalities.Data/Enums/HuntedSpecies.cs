86f6bbc45eea4aa407a7764e662d229e658702b3
JonHodgins <19355502+JonHodgins@users.noreply.github.com>
2022-09-09 11:54:49 -0700

    Add display name attributes for species enums

--- src/WildlifeMortalities.Data/Enums/HuntedSpecies.cs
+++ src/WildlifeMortalities.Data/Enums/HuntedSpecies.cs
@@ -1,19 +1,35 @@
-﻿namespace WildlifeMortalities.Data.Enums;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WildlifeMortalities.Data.Enums;
 
 public enum HuntedSpecies
 {
     Uninitialized = AllSpecies.Uninitialized,
+    [Display(Name = "American Black Bear")]
     AmericanBlackBear = AllSpecies.AmericanBlackBear,
+    [Display(Name = "Barren Ground Caribou")]
     BarrenGroundCaribou = AllSpecies.BarrenGroundCaribou,
+    [Display(Name = "Coyote")]
     Coyote = AllSpecies.Coyote,
+    [Display(Name = "Elk")]
     Elk = AllSpecies.Elk,
+    [Display(Name = "Grey Wolf")]
     GreyWolf = AllSpecies.GreyWolf,
+    [Display(Name = "Grizzly Bear")]
     GrizzlyBear = AllSpecies.GrizzlyBear,
+    [Display(Name = "Moose")]
     Moose = AllSpecies.Moose,
+    [Display(Name = "Mountain Goat")]
     MountainGoat = AllSpecies.MountainGoat,
+    [Display(Name = "Mule Deer")]
     MuleDeer = AllSpecies.MuleDeer,
+    [Display(Name = "Thinhorn Sheep")]
     ThinhornSheep = AllSpecies.ThinhornSheep,
+    [Display(Name = "Wolverine")]
     Wolverine = AllSpecies.Wolverine,
+    [Display(Name = "Wood Bison")]
     WoodBison = AllSpecies.WoodBison,
+    [Display(Name = "Woodland Caribou")]
     WoodlandCaribou = AllSpecies.WoodlandCaribou
 }
--- src/WildlifeMortalities.Data/Enums/TrappedSpecies.cs
+++ src/WildlifeMortalities.Data/Enums/TrappedSpecies.cs
@@ -1,8 +1,12 @@
-﻿namespace WildlifeMortalities.Data.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WildlifeMortalities.Data.Enums;
 
 public enum TrappedSpecies
 {
     Uninitialized = AllSpecies.Uninitialized,
+    [Display(Name = "Grey Wolf")]
     GreyWolf = AllSpecies.GreyWolf,
+    [Display(Name = "Wolverine")]
     Wolverine = AllSpecies.Wolverine
 }
