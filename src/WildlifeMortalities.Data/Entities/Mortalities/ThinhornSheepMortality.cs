3f8bdc0a5b47d7daecb923734449eb4e56110e15
JonHodgins <19355502+JonHodgins@users.noreply.github.com>
2022-09-09 11:53:50 -0700

    Add thinhorn sheep properties

--- src/WildlifeMortalities.Data/Entities/Mortalities/ThinhornSheepMortality.cs
+++ src/WildlifeMortalities.Data/Entities/Mortalities/ThinhornSheepMortality.cs
@@ -1,3 +1,21 @@
 ﻿namespace WildlifeMortalities.Data.Entities.Mortalities;
 
-public class ThinhornSheepMortality : Mortality { }
+public class ThinhornSheepMortality : Mortality {
+    public ThinhornSheepBodyColour BodyColour { get; set; }
+    public ThinhornSheepTailColour TailColour { get; set; }
+}
+
+public enum ThinhornSheepBodyColour
+{
+    Uninitialized = 0,
+    Dark = 1,
+    Fannin = 2,
+    White = 3
+}
+
+public enum ThinhornSheepTailColour
+{
+    Uninitialized = 0,
+    Dark = 1,
+    White = 2
+}
