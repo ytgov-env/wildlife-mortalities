3030fd0b3b69b80eddd27b1bafb0b96296464062
JonHodgins <19355502+JonHodgins@users.noreply.github.com>
2022-09-09 11:54:29 -0700

    Add age property to bio submission entity

--- src/WildlifeMortalities.Data/Entities/BiologicalSubmissions/BioSubmission.cs
+++ src/WildlifeMortalities.Data/Entities/BiologicalSubmissions/BioSubmission.cs
@@ -3,4 +3,19 @@
 public class BioSubmission
 {
     public int Id { get; set; }
+    public Age Age { get; set; } = null!;
+}
+
+public class Age
+{
+    public int Years { get; set; }
+    public ConfidenceInAge Confidence { get; set; }
+}
+
+public enum ConfidenceInAge
+{
+    Uninitialized = 0,
+    Fair = 1,
+    Good = 2,
+    Poor = 3
 }
