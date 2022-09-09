7ac799133c9617f5dbe0f08eafc781cb0dc3b4fb
JonHodgins <19355502+JonHodgins@users.noreply.github.com>
2022-09-09 11:54:10 -0700

    Add black bear bio submission properties

--- src/WildlifeMortalities.Data/Entities/BiologicalSubmissions/AmericanBlackBearBioSubmission.cs
+++ src/WildlifeMortalities.Data/Entities/BiologicalSubmissions/AmericanBlackBearBioSubmission.cs
@@ -4,6 +4,9 @@ namespace WildlifeMortalities.Data.Entities.BiologicalSubmissions;
 
 public class AmericanBlackBearBioSubmission : BioSubmission
 {
+    public string SkullCondition { get; set; } = "";
+    public decimal SkullLength { get; set; }
+    public decimal SkullHeight { get; set; }
     public int MortalityId { get; set; }
     public AmericanBlackBearMortality Mortality { get; set; } = null!;
 }
