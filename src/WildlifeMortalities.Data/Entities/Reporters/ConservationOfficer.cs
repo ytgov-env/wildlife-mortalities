d0e67b0344eef664800b859a0d2d18a6d7fa3b67
JonHodgins <19355502+JonHodgins@users.noreply.github.com>
2022-09-12 16:17:50 -0700

    Add first and last name properties to conservation officer entity

--- src/WildlifeMortalities.Data/Entities/Reporters/ConservationOfficer.cs
+++ src/WildlifeMortalities.Data/Entities/Reporters/ConservationOfficer.cs
@@ -3,4 +3,6 @@
 public class ConservationOfficer : Reporter
 {
     public string BadgeNumber { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
 }
