b6b8fb42358b02929dda1911c82baa380ca56898
JonHodgins <19355502+JonHodgins@users.noreply.github.com>
2022-10-24 12:52:10 -0700

    Rename black bear bio submission skull measurement properties

--- src/WildlifeMortalities.Data/Entities/BiologicalSubmissions/AmericanBlackBearBioSubmission.cs
+++ src/WildlifeMortalities.Data/Entities/BiologicalSubmissions/AmericanBlackBearBioSubmission.cs
@@ -5,8 +5,8 @@ namespace WildlifeMortalities.Data.Entities.BiologicalSubmissions;
 public class AmericanBlackBearBioSubmission : BioSubmission
 {
     public string SkullCondition { get; set; } = "";
-    public decimal SkullLength { get; set; }
-    public decimal SkullHeight { get; set; }
+    public int SkullLengthMillimetre { get; set; }
+    public int SkullHeightMillimetre { get; set; }
     public int MortalityId { get; set; }
     public AmericanBlackBearMortality Mortality { get; set; } = null!;
 }
