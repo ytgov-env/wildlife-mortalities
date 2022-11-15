c8a70beccb15383fc2338e9a2bf9300ad585be11
JonHodgins <19355502+JonHodgins@users.noreply.github.com>
2022-11-14 17:17:32 -0700

    Add missing posseexampleapi files

---
+++ src/WildlifeMortalities.PosseExampleApi/Program.cs
@@ -0,0 +1,45 @@
+using NSwag;
+using WildlifeMortalities.PosseExampleApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument(config =>
+{
+    config.PostProcess = document =>
+    {
+        document.Info.Version = "v1";
+        document.Info.Title = "Posse example API";
+        document.Info.Contact = new OpenApiContact
+        {
+            Name = "Jon Hodgins",
+            Email = "jon.hodgins@yukon.ca",
+            Url = "https://github.com/ytgov-env/wildlife-mortalities"
+        };
+    };
+
+    config.GenerateEnumMappingDescription = true;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseOpenApi();
+    app.UseSwaggerUi3();
+    app.UseReDoc(config =>
+    {
+        config.Path = "/redoc";
+        config.DocumentPath = "/swagger/v1/swagger.json";
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.MapClientEndpoints();
+
+app.Run();
