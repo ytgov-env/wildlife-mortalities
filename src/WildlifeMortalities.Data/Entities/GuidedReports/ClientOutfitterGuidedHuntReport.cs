aacfa9a9bf5143119f28a6c2e1ab9d56af9810ba
JonHodgins <19355502+JonHodgins@users.noreply.github.com>
2022-11-15 15:34:05 -0700

    Remove GMA-associated entities, and explicit join entity for Client-OutfitterGuidedHuntReport

--- src/WildlifeMortalities.Data/Entities/GuidedReports/ClientOutfitterGuidedHuntReport.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WildlifeMortalities.Data.Entities.GuidedReports;
-
-public class ClientOutfitterGuidedHuntReport
-{
-    public int GuideId { get; set; }
-    public int OutfitterGuidedHuntReportId { get; set; }
-}
