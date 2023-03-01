66203c73f49b9f14a1085016f4308a93557d2418
JonHodgins <19355502+JonHodgins@users.noreply.github.com>
2023-03-01 14:27:34 -0700

    Scaffold pdf generation template (wip)

---
+++ src/WildlifeMortalities.PdfTester/Program.cs
@@ -0,0 +1,102 @@
+﻿using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+using QuestPDF.Previewer;
+using WildlifeMortalities.Data;
+
+using var context = new AppDbContext();
+
+var document = Document.Create(container =>
+{
+    container.Page(page =>
+    {
+        page.Size(PageSizes.A4);
+        page.Margin(2, Unit.Centimetre);
+        page.PageColor(Colors.White);
+        page.DefaultTextStyle(x => x.FontFamily(Fonts.Arial).FontSize(20));
+
+        page.Header()
+            .Column(column =>
+            {
+                column
+                    .Item()
+                    .Row(row =>
+                    {
+                        row.RelativeItem()
+                            .Image(
+                                "..\\WildlifeMortalities.Shared\\Resources\\GYWordmark_2018_RGB.png",
+                                ImageScaling.FitHeight
+                            );
+                        row.RelativeItem()
+                            .Text("Harvest report")
+                            .SemiBold()
+                            .FontSize(24)
+                            .FontColor(Colors.Black);
+                    });
+            });
+
+        page.Content()
+            .PaddingVertical(1, Unit.Centimetre)
+            .Column(column =>
+            {
+                column
+                    .Item()
+                    .Row(row =>
+                    {
+                        row.RelativeItem().LabelCell("Label 1");
+
+                        row.RelativeItem(3)
+                            .Grid(grid =>
+                            {
+                                grid.Columns(3);
+
+                                grid.Item(2).LabelCell("Label 2");
+                                grid.Item().LabelCell("Label 3");
+
+                                grid.Item(2).ValueCell().Text("Value 2");
+                                grid.Item().ValueCell().Text("Value 3");
+                            });
+                    });
+
+                column
+                    .Item()
+                    .Row(row =>
+                    {
+                        row.RelativeItem().ValueCell().Text("Value 1");
+
+                        row.RelativeItem(3)
+                            .Grid(grid =>
+                            {
+                                grid.Columns(3);
+
+                                grid.Item().LabelCell("Label 4");
+                                grid.Item(2).LabelCell("Label 5");
+
+                                grid.Item().ValueCell().Text("Value 4");
+                                grid.Item(2).ValueCell().Text("Value 5");
+                            });
+                    });
+
+                column
+                    .Item()
+                    .Row(row =>
+                    {
+                        row.RelativeItem().LabelCell("Label 6");
+                        row.RelativeItem()
+                            .Image(
+                                "..\\WildlifeMortalities.Shared\\Resources\\YG_Environment_Lichen_RGB.png"
+                            );
+                    });
+            });
+
+        page.Footer()
+            .AlignCenter()
+            .Text(x =>
+            {
+                x.Span("Page ");
+                x.CurrentPageNumber();
+            });
+    });
+});
+
+document.ShowInPreviewer();
---
+++ src/WildlifeMortalities.PdfTester/SimpleExtension.cs
@@ -0,0 +1,20 @@
+﻿using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+
+static class SimpleExtension
+{
+    private static IContainer Cell(this IContainer container, bool dark)
+    {
+        return container
+            .Border(1)
+            .Background(dark ? Colors.Grey.Lighten2 : Colors.White)
+            .Padding(10);
+    }
+
+    // displays only text label
+    public static void LabelCell(this IContainer container, string text) => container.Cell(true).Text(text).Medium();
+
+    // allows to inject any type of content, e.g. image
+    public static IContainer ValueCell(this IContainer container) => container.Cell(false);
+}
