7583646b769e1da15e4ff13ecf2b88ac898349c0
JonHodgins <19355502+JonHodgins@users.noreply.github.com>
2023-03-03 12:46:28 -0700

    Hide bison pregnancy select if sex is not female

---
+++ src/WildlifeMortalities.App/Features/Shared/Mortalities/WoodBison/WoodBisonMortalityComponent.razor.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace WildlifeMortalities.App.Features.Shared.Mortalities.WoodBison
+{
+    public partial class WoodBisonMortalityComponent : IDisposable
+    {
+        [CascadingParameter]
+        public EditContext EditContext { get; set; } = null!;
+
+        [Parameter]
+        public WoodBisonMortalityViewModel ViewModel { get; set; } = default!;
+
+        public void Dispose()
+        {
+            if (EditContext != null)
+            {
+                EditContext.OnFieldChanged -= EditContext_OnFieldChanged;
+            }
+        }
+
+        protected override void OnInitialized()
+        {
+            EditContext.OnFieldChanged += EditContext_OnFieldChanged;
+        }
+
+        private void EditContext_OnFieldChanged(object? sender, FieldChangedEventArgs e)
+        {
+            if (e.FieldIdentifier.FieldName == nameof(MortalityViewModel.Sex))
+            {
+                if (ViewModel.Sex != Data.Enums.Sex.Female)
+                {
+                    ViewModel.PregnancyStatus = null;
+                }
+
+                StateHasChanged();
+            }
+        }
+    }
+}
