e200cf1f7e90668e93ac86c13b75f59b25bbfd91
JonHodgins <19355502+JonHodgins@users.noreply.github.com>
2023-04-20 19:15:48 -0700

    Use constants for all routes

--- src/WildlifeMortalities.App/Constants.cs
+++ src/WildlifeMortalities.App/Constants.cs
@@ -16,42 +16,61 @@ public static class Constants
 
     public static class Routes
     {
-        #region Report Routes
-        public const string ReportDetailsPage =
-            "/mortality-reports/{humanReadablePersonId}/{reportId:int}";
+        public const string HomePage = "/";
+
+        public static string GetHomePageLink() => HomePage;
+
+        #region report routes
+        public const string ReportsOverviewPage = "reports";
+
+        public static string GetReportsOverviewPageLink() => ReportsOverviewPage;
+
+        public const string ReportDetailsPage = "reports/{humanReadablePersonId}/{reportId:int}";
 
         public static string GetReportDetailsPageLink(string humanReadablePersonId, int reportId) =>
-            $"/mortality-reports/{humanReadablePersonId}/{reportId}";
+            $"reports/{humanReadablePersonId}/{reportId}";
 
         public const string CreateReportPage = "/mortality-reports/{humanReadablePersonId}/new";
 
         public static string GetCreateReportPageLink(string humanReadablePersonId) =>
-            $"/mortality-reports/{humanReadablePersonId}/new";
+            $"reports/{humanReadablePersonId}/new";
 
-        public const string EditReportPage =
-            "/mortality-reports/{humanReadablePersonId}/edit/{reportId:int}";
+        public const string EditReportPage = "reports/{humanReadablePersonId}/edit/{reportId:int}";
 
         public static string GetEditReportPageLink(string humanReadablePersonId, int reportId) =>
-            $"/mortality-reports/{humanReadablePersonId}/edit/{reportId}";
+            $"reports/{humanReadablePersonId}/edit/{reportId}";
 
         public const string EditDraftReportPage =
-            "/mortality-reports/{humanReadablePersonId}/editdraft/{draftId:int}";
+            "reports/{humanReadablePersonId}/editdraft/{draftId:int}";
 
         public static string GetEditDraftReportPageLink(
             string humanReadablePersonId,
             int reportId
-        ) => $"/mortality-reports/{humanReadablePersonId}/editdraft/{reportId}";
+        ) => $"reports/{humanReadablePersonId}/editdraft/{reportId}";
         #endregion
 
-        #region Client Routes
-        public const string ClientOverviewPage = "/clients/{envClientId}";
-
-        public static string GetClientOverviewPageLink(string envClientId) =>
-            $"reporters/clients/{envClientId}";
-
+        #region client routes
         public const string ClientLookupPage = "reporters/clients";
 
         public static string GetClientLookupPageLink() => ClientLookupPage;
+
+        public const string ClientOverviewPage = "reporters/clients/{envClientId}";
+
+        public static string GetClientOverviewPageLink(string envClientId) =>
+            $"reporters/clients/{envClientId}";
+        #endregion
+
+        #region conservation officer routes
+        public const string ConservationOfficerLookupPage = "reporters/conservation-officers";
+
+        public static string GetConservationOfficerLookupPageLink() =>
+            ConservationOfficerLookupPage;
+
+        public const string ConservationOfficerOverviewPage =
+            "reporters/conservation-officers/{badgeNumber}";
+
+        public static string GetConservationOfficerOverviewPageLink(string badgeNumber) =>
+            $"reporters/conservation-officers/{badgeNumber}";
         #endregion
     }
 }
