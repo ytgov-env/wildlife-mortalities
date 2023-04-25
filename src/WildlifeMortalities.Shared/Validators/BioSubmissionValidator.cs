c7a51b21b2cf2b36df4ecf44603144622bb9a555
JonHodgins <19355502+JonHodgins@users.noreply.github.com>
2023-04-25 10:21:30 -0700

    Remove validators from shared project

--- src/WildlifeMortalities.Shared/Validators/BioSubmissionValidator.cs
+++
@@ -1,6 +0,0 @@
-﻿using WildlifeMortalities.Data.Entities.BiologicalSubmissions;
-
-namespace WildlifeMortalities.Shared.Validators;
-
-public class BioSubmissionValidator<T> : AbstractValidator<BioSubmission>
-    where T : BioSubmission { }
