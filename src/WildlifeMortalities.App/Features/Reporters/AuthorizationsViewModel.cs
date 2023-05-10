f6ed945589e2bddc1167ceba054a1dc6e1a876ab
JonHodgins <19355502+JonHodgins@users.noreply.github.com>
2023-05-10 14:40:19 -0700

    Display invalid authorizations that have a season (no season still to be implemented)

--- src/WildlifeMortalities.App/Features/Reporters/AuthorizationsViewModel.cs
+++ src/WildlifeMortalities.App/Features/Reporters/AuthorizationsViewModel.cs
@@ -5,4 +5,6 @@ namespace WildlifeMortalities.App.Features.Reporters;
 public class AuthorizationsViewModel
 {
     public IEnumerable<Authorization> Authorizations { get; set; } = Array.Empty<Authorization>();
+    public IEnumerable<InvalidAuthorization> InvalidAuthorizations { get; set; } =
+        Array.Empty<InvalidAuthorization>();
 }
