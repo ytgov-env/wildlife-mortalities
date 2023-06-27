8f38097cbecd90a86a03e3791874984a1b9878fa
JonHodgins <19355502+JonHodgins@users.noreply.github.com>
2023-06-27 09:24:28 -0700

    Add unit tests for generic authorization rule and big game hunting licence

---
+++ src/WildlifeMortalities.Test/Rules/Authorizations/BigGameHuntingLicenceTester.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using WildlifeMortalities.Data.Entities.Reports.SingleMortality;
+using WildlifeMortalities.Data;
+using Xunit;
+using WildlifeMortalities.Data.Rules.Authorizations;
+using WildlifeMortalities.Data.Entities.Mortalities;
+using WildlifeMortalities.Data.Entities.Reports.MultipleMortalities;
+using WildlifeMortalities.Data.Entities.People;
+using static WildlifeMortalities.Data.Entities.Violation;
+using WildlifeMortalities.Data.Entities;
+using WildlifeMortalities.Data.Entities.Authorizations;
+using WildlifeMortalities.Data.Entities.Seasons;
+using WildlifeMortalities.Data.Entities.Reports;
+
+namespace WildlifeMortalities.Test.Rules.Authorizations;
+
+public class BigGameHuntingLicenceTester
+{
+    [Fact]
+    public async Task AuthorizationRule_IsNotBigGame_IsNotApplicable()
+    {
+        var dbContext = new AppDbContext();
+
+        var report = new IndividualHuntedMortalityReport()
+        {
+            HuntedActivity = new HuntedActivity()
+            {
+                Mortality = new ArcticGroundSquirrelMortality()
+            }
+        };
+
+        var item = new BigGameHuntingLicenceRulePipelineItem();
+
+        var context = new AuthorizationRulePipelineContext(dbContext);
+
+        var result = await item.Process(report, context);
+
+        result.Should().BeTrue();
+        context.Violations.Should().BeEmpty();
+        context.RelevantAuthorizations.Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task AuthorizationRule_Trapping_IsNotApplicable()
+    {
+        var dbContext = new AppDbContext();
+
+        var report = new TrappedMortalitiesReport();
+
+        var item = new BigGameHuntingLicenceRulePipelineItem();
+
+        var context = new AuthorizationRulePipelineContext(dbContext);
+
+        var result = await item.Process(report, context);
+
+        result.Should().BeTrue();
+        context.Violations.Should().BeEmpty();
+        context.RelevantAuthorizations.Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task AuthorizationRule_WithNoAuthorization_IsIllegal()
+    {
+        var dbContext = new AppDbContext();
+
+        var report = new IndividualHuntedMortalityReport()
+        {
+            Person = new Client() { Authorizations = new() },
+            HuntedActivity = new HuntedActivity()
+            {
+                Mortality = new AmericanBlackBearMortality()
+                {
+                    DateOfDeath = new DateTimeOffset(2023, 4, 1, 0, 0, 0, TimeSpan.Zero)
+                }
+            }
+        };
+
+        var item = new BigGameHuntingLicenceRulePipelineItem();
+
+        var context = new AuthorizationRulePipelineContext(dbContext);
+
+        var result = await item.Process(report, context);
+
+        result.Should().BeTrue();
+        context.Violations.Should().ContainSingle();
+        var violation = context.Violations.First();
+
+        violation
+            .Should()
+            .BeEquivalentTo(
+                new Violation(
+                    report.GetActivities().First(),
+                    RuleType.Authorization,
+                    SeverityType.Illegal,
+                    "No big game hunting licence valid on 2023-04-01."
+                )
+            );
+
+        context.RelevantAuthorizations.Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task AuthorizationRule_WithAuthorization_IsLegal()
+    {
+        var dbContext = new AppDbContext();
+        var dateOfDeath = new DateTimeOffset(2023, 6, 1, 0, 0, 0, TimeSpan.FromHours(-7));
+        var validStart = dateOfDeath.AddDays(-10);
+        var validEnd = dateOfDeath.AddDays(10);
+
+        var report = new IndividualHuntedMortalityReport()
+        {
+            Person = new Client()
+            {
+                Authorizations = new()
+                {
+                    new BigGameHuntingLicence()
+                    {
+                        Season = new HuntingSeason(2023),
+                        ValidFromDateTime = validStart,
+                        ValidToDateTime = validEnd
+                    }
+                }
+            },
+            HuntedActivity = new HuntedActivity()
+            {
+                Mortality = new AmericanBlackBearMortality() { DateOfDeath = dateOfDeath }
+            }
+        };
+
+        var item = new BigGameHuntingLicenceRulePipelineItem();
+
+        var context = new AuthorizationRulePipelineContext(dbContext);
+
+        var result = await item.Process(report, context);
+
+        result.Should().BeTrue();
+        context.Violations.Should().BeEmpty();
+        context.RelevantAuthorizations.Should().ContainSingle();
+    }
+
+    [Theory]
+    [InlineData(BigGameHuntingLicence.LicenceType.CanadianResident)]
+    [InlineData(BigGameHuntingLicence.LicenceType.CanadianResidentSpecialGuided)]
+    public async Task AuthorizationRule_WithCanadianResidentBigGameHuntingLicenceAndNoGuide_IsIllegal(
+        BigGameHuntingLicence.LicenceType licenceType
+    )
+    {
+        var dbContext = new AppDbContext();
+        var dateOfDeath = new DateTimeOffset(2023, 6, 1, 0, 0, 0, TimeSpan.FromHours(-7));
+        var validStart = dateOfDeath.AddDays(-10);
+        var validEnd = dateOfDeath.AddDays(10);
+
+        var report = new IndividualHuntedMortalityReport()
+        {
+            Person = new Client()
+            {
+                Authorizations = new()
+                {
+                    new BigGameHuntingLicence()
+                    {
+                        Season = new HuntingSeason(2023),
+                        ValidFromDateTime = validStart,
+                        ValidToDateTime = validEnd,
+                        Type = licenceType
+                    }
+                }
+            },
+            HuntedActivity = new HuntedActivity()
+            {
+                Mortality = new AmericanBlackBearMortality() { DateOfDeath = dateOfDeath }
+            }
+        };
+
+        var item = new BigGameHuntingLicenceRulePipelineItem();
+
+        var context = new AuthorizationRulePipelineContext(dbContext);
+
+        var result = await item.Process(report, context);
+
+        result.Should().BeTrue();
+        context.Violations.Should().ContainSingle();
+        var violation = context.Violations.First();
+
+        violation
+            .Should()
+            .BeEquivalentTo(
+                new Violation(
+                    report.GetActivities().First(),
+                    RuleType.Authorization,
+                    SeverityType.Illegal,
+                    "Hunted big game as a Canadian resident without a guide."
+                )
+            );
+        context.RelevantAuthorizations.Should().ContainSingle();
+    }
+
+    [Theory]
+    [InlineData(BigGameHuntingLicence.LicenceType.CanadianResident)]
+    [InlineData(BigGameHuntingLicence.LicenceType.CanadianResidentSpecialGuided)]
+    public async Task AuthorizationRule_WithCanadianResidentBigGameHuntingLicenceAndGuided_IsLegal(
+        BigGameHuntingLicence.LicenceType licenceType
+    )
+    {
+        var dbContext = new AppDbContext();
+        var dateOfDeath = new DateTimeOffset(2023, 6, 1, 0, 0, 0, TimeSpan.FromHours(-7));
+        var validStart = dateOfDeath.AddDays(-10);
+        var validEnd = dateOfDeath.AddDays(10);
+
+        Report report = null;
+        if (licenceType == BigGameHuntingLicence.LicenceType.CanadianResident)
+        {
+            report = new OutfitterGuidedHuntReport
+            {
+                Client = new Client()
+                {
+                    Authorizations = new()
+                    {
+                        new BigGameHuntingLicence()
+                        {
+                            Season = new HuntingSeason(2023),
+                            ValidFromDateTime = validStart,
+                            ValidToDateTime = validEnd,
+                            Type = licenceType
+                        }
+                    }
+                },
+                HuntedActivities = new()
+                {
+                    new HuntedActivity()
+                    {
+                        Mortality = new AmericanBlackBearMortality() { DateOfDeath = dateOfDeath }
+                    }
+                }
+            };
+        }
+        else if (licenceType == BigGameHuntingLicence.LicenceType.CanadianResidentSpecialGuided)
+        {
+            report = new SpecialGuidedHuntReport
+            {
+                Client = new Client()
+                {
+                    Authorizations = new()
+                    {
+                        new BigGameHuntingLicence()
+                        {
+                            Season = new HuntingSeason(2023),
+                            ValidFromDateTime = validStart,
+                            ValidToDateTime = validEnd,
+                            Type = licenceType
+                        }
+                    }
+                },
+                HuntedActivities = new()
+                {
+                    new HuntedActivity()
+                    {
+                        Mortality = new AmericanBlackBearMortality() { DateOfDeath = dateOfDeath }
+                    }
+                }
+            };
+        }
+
+        var item = new BigGameHuntingLicenceRulePipelineItem();
+
+        var context = new AuthorizationRulePipelineContext(dbContext);
+
+        var result = await item.Process(report!, context);
+
+        result.Should().BeTrue();
+        context.Violations.Should().BeEmpty();
+        context.RelevantAuthorizations.Should().ContainSingle();
+    }
+}
---
+++ src/WildlifeMortalities.Test/Rules/Authorizations/GenericTester.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Diagnostics;
+using WildlifeMortalities.Data;
+using WildlifeMortalities.Data.Entities;
+using WildlifeMortalities.Data.Entities.Authorizations;
+using WildlifeMortalities.Data.Entities.Reports;
+using WildlifeMortalities.Data.Entities.Reports.SingleMortality;
+using WildlifeMortalities.Data.Rules.Authorizations;
+using Xunit;
+
+namespace WildlifeMortalities.Test.Rules.Authorizations;
+
+public class GenericTester
+{
+    #region DummyClasses
+
+    public class DummyAuthorization : Authorization
+    {
+        public override string GetAuthorizationType()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void UpdateInternal(Authorization authorization)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class DummyAuthorizationRulePipelineItemReturnTrueAndUseAuthorization
+        : AuthorizationRulePipelineItem
+    {
+        public override Task<bool> Process(
+            Report report,
+            AuthorizationRulePipelineContext pipelineContext
+        )
+        {
+            pipelineContext.RelevantAuthorizations.Add(new DummyAuthorization());
+
+            return Task.FromResult(true);
+        }
+    }
+
+    public class DummyAuthorizationRulePipelineItemReturnTrueAndNoAuthorization
+        : AuthorizationRulePipelineItem
+    {
+        public override Task<bool> Process(
+            Report report,
+            AuthorizationRulePipelineContext pipelineContext
+        )
+        {
+            return Task.FromResult(true);
+        }
+    }
+
+    public class DummyAuthorizationRulePipelineItemReturnTrueHasViolation
+        : AuthorizationRulePipelineItem
+    {
+        public override Task<bool> Process(
+            Report report,
+            AuthorizationRulePipelineContext pipelineContext
+        )
+        {
+            pipelineContext.Violations.Add(
+                new Violation(
+                    new HuntedActivity(),
+                    Violation.RuleType.Authorization,
+                    Violation.SeverityType.Illegal,
+                    "test"
+                )
+            );
+
+            return Task.FromResult(true);
+        }
+    }
+
+    public class DummyAuthorizationRulePipelineItemReturnFalse : AuthorizationRulePipelineItem
+    {
+        public override Task<bool> Process(
+            Report report,
+            AuthorizationRulePipelineContext pipelineContext
+        )
+        {
+            return Task.FromResult(false);
+        }
+    }
+
+    public class DummyAuthorizationRulePipelineItemThrowsException : AuthorizationRulePipelineItem
+    {
+        public override Task<bool> Process(
+            Report report,
+            AuthorizationRulePipelineContext pipelineContext
+        )
+        {
+            throw new Exception();
+        }
+    }
+
+    public class DummyAuthorizationRulePipeline : AuthorizationRulePipeline
+    {
+        public DummyAuthorizationRulePipeline(
+            AuthorizationRulePipelineContext context,
+            params AuthorizationRulePipelineItem[] rules
+        )
+            : base(context)
+        {
+            Items = rules;
+        }
+    }
+
+    #endregion
+
+    [Fact]
+    public async Task AuthorizationRule_WithNoViolationsAndUsedAuthorizations_IsLegal()
+    {
+        var context = new AppDbContext();
+
+        var report = new IndividualHuntedMortalityReport();
+
+        var authorizationRule = new AuthorizationRule(
+            x =>
+                new DummyAuthorizationRulePipeline(
+                    x,
+                    new DummyAuthorizationRulePipelineItemReturnTrueAndUseAuthorization()
+                )
+        );
+
+        var result = await authorizationRule.Process(report, context);
+
+        result.Violations.Should().BeEmpty();
+        result.IsApplicable.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task AuthorizationRule_WithNoViolationsAndNoAuthorization_IsNotApplicable()
+    {
+        var context = new AppDbContext();
+
+        var report = new IndividualHuntedMortalityReport();
+
+        var authorizationRule = new AuthorizationRule(
+            x =>
+                new DummyAuthorizationRulePipeline(
+                    x,
+                    new DummyAuthorizationRulePipelineItemReturnTrueAndNoAuthorization()
+                )
+        );
+
+        var result = await authorizationRule.Process(report, context);
+
+        result.Violations.Should().BeEmpty();
+        result.IsApplicable.Should().BeFalse();
+    }
+
+    [Fact]
+    public async Task AuthorizationRule_WithViolation_IsIllegal()
+    {
+        var context = new AppDbContext();
+
+        var report = new IndividualHuntedMortalityReport();
+
+        var authorizationRule = new AuthorizationRule(
+            x =>
+                new DummyAuthorizationRulePipeline(
+                    x,
+                    new DummyAuthorizationRulePipelineItemReturnTrueHasViolation()
+                )
+        );
+
+        var result = await authorizationRule.Process(report, context);
+
+        result.Violations.Should().ContainSingle();
+    }
+
+    [Fact]
+    public async Task AuthorizationRule_WithMultipleViolations_IsIllegal()
+    {
+        var context = new AppDbContext();
+
+        var report = new IndividualHuntedMortalityReport();
+
+        var authorizationRule = new AuthorizationRule(
+            x =>
+                new DummyAuthorizationRulePipeline(
+                    x,
+                    new DummyAuthorizationRulePipelineItemReturnTrueHasViolation(),
+                    new DummyAuthorizationRulePipelineItemReturnTrueHasViolation()
+                )
+        );
+
+        var result = await authorizationRule.Process(report, context);
+
+        result.Violations.Should().HaveCount(2);
+    }
+
+    [Fact]
+    public async Task AuthorizationRule_PipelineStopsOnFalse_IsNotApplicable()
+    {
+        var context = new AppDbContext();
+
+        var report = new IndividualHuntedMortalityReport();
+
+        var authorizationRule = new AuthorizationRule(
+            x =>
+                new DummyAuthorizationRulePipeline(
+                    x,
+                    new DummyAuthorizationRulePipelineItemReturnFalse(),
+                    new DummyAuthorizationRulePipelineItemThrowsException()
+                )
+        );
+
+        var result = await authorizationRule.Process(report, context);
+
+        result.Violations.Should().BeEmpty();
+        result.IsApplicable.Should().BeFalse();
+    }
+}
