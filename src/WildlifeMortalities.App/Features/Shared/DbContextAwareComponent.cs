9a729a9d4f86adbb895d1d002a5d7b5f8fa68145
JonHodgins <19355502+JonHodgins@users.noreply.github.com>
2023-07-06 12:23:31 -0700

    Fix dbcontext disposal bug

--- src/WildlifeMortalities.App/Features/Shared/DbContextAwareComponent.cs
+++ src/WildlifeMortalities.App/Features/Shared/DbContextAwareComponent.cs
@@ -8,15 +8,24 @@ public class DbContextAwareComponent : ComponentBase, IDisposable
 {
     private bool _disposedValue;
 
-    protected ReadOnlyAppDbContext Context { get; private set; } = null!;
+    private ReadOnlyAppDbContext? _context;
+
+    // Using an in memory database if _context is null is a workaround to avoid exceptions that terminate the circuit
+    // when a user navigates away from a page while async lifecycle methods are still running. The framework can call
+    // dispose at any time.
+    protected ReadOnlyAppDbContext Context =>
+        _context
+        ?? new ReadOnlyAppDbContext(
+            new DbContextOptionsBuilder<AppDbContext>().UseInMemoryDatabase("unused").Options
+        );
 
     [Inject]
     private IDbContextFactory<ReadOnlyAppDbContext> DbContextFactory { get; set; } = null!;
 
     protected override void OnInitialized()
     {
-        Context = DbContextFactory.CreateDbContext();
-        Context.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        _context = DbContextFactory.CreateDbContext();
+        _context.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
     }
 
     protected virtual void Dispose(bool disposing)
@@ -25,7 +34,8 @@ public class DbContextAwareComponent : ComponentBase, IDisposable
         {
             if (disposing)
             {
-                Context?.Dispose();
+                _context?.Dispose();
+                _context = null;
             }
 
             _disposedValue = true;
