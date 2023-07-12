159ce96cd8fbeaa7bfb3b2efb41419e75f82abad
JonHodgins <19355502+JonHodgins@users.noreply.github.com>
2023-07-11 17:58:43 -0700

    Add migration for bb55cf9f

---
+++ src/WildlifeMortalities.Data/Migrations/20230712005757_RenameRTCAreaToConcession.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WildlifeMortalities.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameRTCAreaToConcession : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Area",
+                table: "RegisteredTrappingConcessions",
+                newName: "Concession");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Concession",
+                table: "RegisteredTrappingConcessions",
+                newName: "Area");
+        }
+    }
+}
