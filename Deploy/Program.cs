662729da328b40b24a48b73fab471c08b88b8d46
JonHodgins <19355502+JonHodgins@users.noreply.github.com>
2023-08-01 13:27:07 -0700

    Debugging

--- Deploy/Program.cs
+++ Deploy/Program.cs
@@ -1,6 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Moq;
 using WildlifeMortalities.App.Extensions;
 using WildlifeMortalities.Data;
+using WildlifeMortalities.Data.Entities.Reports.SingleMortality;
+using WildlifeMortalities.Data.Entities.Rules.BagLimit;
+using WildlifeMortalities.Data.Enums;
+using WildlifeMortalities.Shared.Services;
+using WildlifeMortalities.Shared.Services.Reports.Single;
 
 var options = new DbContextOptionsBuilder<AppDbContext>()
     .UseSqlServer(
@@ -11,26 +18,76 @@ var options = new DbContextOptionsBuilder<AppDbContext>()
 
 using var setupContext = new AppDbContext(options);
 
-//var reports = await setupContext.Reports.WithEntireGraph().AsNoTracking().ToListAsync();
-var users = await setupContext.Users.Include(x => x.Permissions).ToListAsync();
+//var users = await setupContext.Users.Include(x => x.Permissions).ToListAsync();
 
-var permissions = PermissionsHelper
-    .GetPermissions()
-    .SelectMany(x => x.Permissions)
-    .Select(x => new WildlifeMortalities.Data.Entities.Users.Permission { Value = x.Value })
-    .ToList();
+//var permissions = PermissionsHelper
+//    .GetPermissions()
+//    .SelectMany(x => x.Permissions)
+//    .Select(x => new WildlifeMortalities.Data.Entities.Users.Permission { Value = x.Value })
+//    .ToList();
 
-foreach (var item in permissions)
+//foreach (var item in permissions)
+//{
+//    setupContext.Permissions.AddRange(permissions);
+//}
+
+//foreach (var user in users)
+//{
+//    user.Permissions.AddRange(permissions);
+//}
+
+//await setupContext.SaveChangesAsync();
+
+var reports = await setupContext.Reports.WithEntireGraph().AsNoTracking().ToListAsync();
+
+var temp1 = await setupContext.Activities
+    .OfType<HarvestActivity>()
+    .Where(x => x.ActivityQueueItem == null || x.ActivityQueueItem.BagLimitEntry == null)
+    .ToListAsync();
+
+var bagEntries = await setupContext.BagEntries
+    .Where(x => x.SharedValue == 0)
+    .SumAsync(x => x.CurrentValue);
+
+var bagLimitEntries = await setupContext.BagLimitEntries
+    .Include(x => x.MaxValuePerPersonSharedWith)
+    .Include(x => x.ActivityQueue)
+    .ThenInclude(x => x.Activity)
+    .ThenInclude(x => x.Mortality)
+    .Include(x => ((HuntingBagLimitEntry)x).Areas)
+    .Include(x => ((HuntingBagLimitEntry)x).Season)
+    .Include(x => ((TrappingBagLimitEntry)x).Concessions)
+    .AsSplitQuery()
+    .ToArrayAsync();
+
+Dictionary<int, List<(int baglimitEntryId, Species species)>> counters = reports
+    .Select(x => x.GetPerson().Id)
+    .Distinct()
+    .ToDictionary(x => x, x => new List<(int, Species)>());
+
+foreach (var item in reports)
 {
-    setupContext.Permissions.AddRange(permissions);
+    foreach (var activity in item.GetActivities().OfType<HarvestActivity>())
+    {
+        var match = bagLimitEntries.FirstOrDefault(x => x.Matches(activity, item));
+        if (match == null)
+        {
+            throw new Exception();
+        }
+
+        counters[item.GetPerson().Id].Add((match.Id, activity.Mortality.Species));
+    }
 }
 
-foreach (var user in users)
+foreach (var item in counters)
 {
-    user.Permissions.AddRange(permissions);
-}
+    var groupedSpecies = item.Value.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
 
-await setupContext.SaveChangesAsync();
+    if (groupedSpecies.Any(x => x.Value > 1))
+    {
+        var a = 4;
+    }
+}
 
 //foreach (var report in reports)
 //{
@@ -40,3 +97,5 @@ await setupContext.SaveChangesAsync();
 //    var service = new MortalityService(contextFactory.Object);
 //    await service.UpdateReport(report, 1);
 //}
+
+Console.ReadLine();
