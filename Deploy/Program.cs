18dd4c07e71f11bc099ea804807491df986c2308
JonHodgins <19355502+JonHodgins@users.noreply.github.com>
2023-08-01 11:44:53 -0700

    Add deploy seed logic

---
+++ Deploy/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WildlifeMortalities.App.Extensions;
+using WildlifeMortalities.Data;
+
+var options = new DbContextOptionsBuilder<AppDbContext>()
+    .UseSqlServer(
+        "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=EnvWildlifeMortalities;Integrated Security=True;"
+    )
+    .EnableSensitiveDataLogging()
+    .Options;
+
+using var setupContext = new AppDbContext(options);
+
+//var reports = await setupContext.Reports.WithEntireGraph().AsNoTracking().ToListAsync();
+var users = await setupContext.Users.Include(x => x.Permissions).ToListAsync();
+
+var permissions = PermissionsHelper
+    .GetPermissions()
+    .SelectMany(x => x.Permissions)
+    .Select(x => new WildlifeMortalities.Data.Entities.Users.Permission { Value = x.Value })
+    .ToList();
+
+foreach (var item in permissions)
+{
+    setupContext.Permissions.AddRange(permissions);
+}
+
+foreach (var user in users)
+{
+    user.Permissions.AddRange(permissions);
+}
+
+await setupContext.SaveChangesAsync();
+
+//foreach (var report in reports)
+//{
+//    using var context = new AppDbContext(options);
+//    var contextFactory = new Mock<IDbContextFactory<AppDbContext>>();
+//    contextFactory.Setup(contextFactory => contextFactory.CreateDbContext()).Returns(context);
+//    var service = new MortalityService(contextFactory.Object);
+//    await service.UpdateReport(report, 1);
+//}
