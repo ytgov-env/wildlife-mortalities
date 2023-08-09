2176907e336fc879d3cc8ca62a4f7f698cb2521a
JonHodgins <19355502+JonHodgins@users.noreply.github.com>
2023-08-09 10:29:03 -0700

    Fix: type not found exception when creating report from draft

--- src/WildlifeMortalities.App/Features/Shared/Mortalities/MortalityWithSpeciesSelectionViewModel.cs
+++ src/WildlifeMortalities.App/Features/Shared/Mortalities/MortalityWithSpeciesSelectionViewModel.cs
@@ -1,9 +1,13 @@
-﻿using WildlifeMortalities.Data.Enums;
+﻿using System.Text.Json.Serialization;
+using WildlifeMortalities.Data;
+using WildlifeMortalities.Data.Enums;
 
 namespace WildlifeMortalities.App.Features.Shared.Mortalities;
 
 public class MortalityWithSpeciesSelectionViewModel
 {
     public Species? Species { get; set; }
+
+    [JsonConverter(typeof(MostConcreteClassJsonConverter<MortalityViewModel>))]
     public MortalityViewModel MortalityViewModel { get; set; }
 }
