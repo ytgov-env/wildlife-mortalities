9e886a9b69d66e588e76ec8ea445f7f3ae5c78d8
JonHodgins <19355502+JonHodgins@users.noreply.github.com>
2023-08-14 00:52:54 -0700

    Fix: circular reference exception when serializing deleted report

--- src/WildlifeMortalities.Data/Entities/People/OutfitterGuide.cs
+++ src/WildlifeMortalities.Data/Entities/People/OutfitterGuide.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.EntityFrameworkCore;
 using WildlifeMortalities.Data.Entities.Reports.MultipleMortalities;
+using System.Text.Json.Serialization;
 
 namespace WildlifeMortalities.Data.Entities.People;
 
@@ -10,8 +11,12 @@ public class OutfitterGuide
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
     public int? ReportAsChiefGuideId { get; set; }
+
+    [JsonIgnore]
     public OutfitterGuidedHuntReport? ReportAsChiefGuide { get; set; }
     public int? ReportAsAssistantGuideId { get; set; }
+
+    [JsonIgnore]
     public OutfitterGuidedHuntReport? ReportAsAssistantGuide { get; set; }
 }
 
